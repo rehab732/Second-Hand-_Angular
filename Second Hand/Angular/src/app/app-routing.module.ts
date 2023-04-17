e949c5d70abf24859ba578b4dbadbaab028de5f7
Rehab Mohamed <75070096+rehab732@users.noreply.github.com>
2023-04-17 22:23:07 +0200

    add login and signup component

--- Second Hand/Angular/src/app/app-routing.module.ts
+++ Second Hand/Angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"signup",component:RegisterComponent},
+  {path:"login",component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
