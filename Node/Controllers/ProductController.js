e89db5ae6554fbcc3465fe49337ba17fcc801e9e
HassanAbdelbaset <hassanelfalt60@gmail.com>
2023-05-08 00:56:42 +0300

    env production and pagination backEnd

--- Node/Controllers/ProductController.js
+++ Node/Controllers/ProductController.js
@@ -109,10 +109,17 @@ let GetProductByName = async (req,res)=>{
 
 
 let GetAllProducts = async (req,res)=>{
+    const {page=1, limit=10} = req.query;
+    const skip = (page - 1) * limit;
    //DB
-    let allProducts= await ProductModel.find().exec();//From DB
+    let allProducts= await ProductModel.find().skip(skip).limit(limit);
+    const totalProducts = await ProductModel.countDocuments();
+    //await ProductModel.find().exec();//From DB
+    
+    
+    
     if(!allProducts||allProducts.length==0) return res.status(401).json({message:"No Products found"});
-    res.status(200).json({message:"Products found",data:allProducts})
+    res.status(200).json({message:"Products found",data:allProducts, page, limit, totalProducts});
 
 
 }
