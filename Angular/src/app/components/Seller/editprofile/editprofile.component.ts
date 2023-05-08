37e1f915146d07e6bb71fb42b01c1622f055c509
Basma Abdelaziz <basbosa.zizo2000@outlook.com>
2023-05-08 22:42:02 +0300

    AddProfileValidation

--- Angular/src/app/components/Seller/editprofile/editprofile.component.ts
+++ Angular/src/app/components/Seller/editprofile/editprofile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { flip } from '@popperjs/core';
 import jwtDecode from 'jwt-decode';
 import { CustomerService } from 'src/app/Services/Customers.service';
 @Component({
@@ -11,6 +12,14 @@ export class EditprofileComponent implements OnInit {
 
   token:any;
   customerID:any;
+
+  editDisabled = false;
+  notValid = false;
+  isNull = false;
+
+  addIsNull = false;
+  Edited = false;
+  added = false;
   getDecodedAccessToken(token: string): any {
     try {
       return jwtDecode(token);
@@ -30,6 +39,13 @@ export class EditprofileComponent implements OnInit {
 
 Add(Apart:any,Floor:any,Street:any,Zone:any,City:any,Gover:any)
 {
+  if(!Apart || !Floor || !Street || !Zone || !City || !Gover)
+  {
+    this.addIsNull = true;
+  }
+  else{
+    this.addIsNull = false;
+
   let newaddress =  {
             ApartmentNumber:+Apart,
             FloorNumber:+Floor,
@@ -43,17 +59,36 @@ Add(Apart:any,Floor:any,Street:any,Zone:any,City:any,Gover:any)
       {
         next:(data)=>{
            console.log("customerService.AddnewAddress" , data);
+           this.added = true;
         },
         error:(err)=>{
           console.error("errrrrrrrrrrrrror");
           console.error(err)}
       }
     );
+  }
 }
 
 
 Edit(name:any,phone:any,email:any,dofb:any)
 {
+  if(!this.customerdetails.Name|| !this.customerdetails.Email || !this.customerdetails.Phone
+    ||!this.customerdetails.DateOfBirth)
+  {
+    this.isNull = true;
+    this.notValid = false;
+  }
+  else if(this.customerdetails.Name.length>50 || this.customerdetails.Name.length<2 ||
+    !this.customerdetails.Email.includes('@') || !this.customerdetails.Email.includes('.'))
+    {
+      this.isNull = false;
+      this.notValid = true;
+    }
+    else{
+      this.isNull = false;
+      this.notValid =false;
+
+
   let editedCustomer={
     Name:name,
     Phone:phone,
@@ -64,14 +99,14 @@ Edit(name:any,phone:any,email:any,dofb:any)
 this.customerService.editCustomerProfile(editedCustomer,this.customerID).subscribe(
   {
     next:(data)=>{
-      console.log(data);
+      this.Edited = true;
     },
     error:(err)=>{
       console.error("up-error" , err.message);
     }
   }
 )
-
+    }
 }
 
   ngOnInit(): void {
