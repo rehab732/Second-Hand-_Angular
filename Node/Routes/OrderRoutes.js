5412e6d12ef57d2bec09920f12c9275b28b0c264
Sarah Ali <saraali23232323@gmail.com.com>
2023-05-08 19:31:08 +0300

    permession

--- Node/Routes/OrderRoutes.js
+++ Node/Routes/OrderRoutes.js
@@ -6,7 +6,7 @@ const loginPermission = require("../Middlewares/loginPermission");
 router.post("/add", loginPermission, OrderController.AddNewOrder);
 router.get("/get/:id", loginPermission, OrderController.GetOrderById);
 router.get("/get", loginPermission, OrderController.GetAllOrders);
-router.get("/getSeller/:id", OrderController.GetSellerOrders);
+router.get("/getSeller/:id",loginPermission, OrderController.GetSellerOrders);
 // router.put("status/:id",OrderController.updateOrderStatus);
 router.get("/get/buyer/:id", loginPermission, OrderController.GetBuyerOrders);
 router.put("/update/:id",loginPermission,OrderController.updateOrderItemRating)
